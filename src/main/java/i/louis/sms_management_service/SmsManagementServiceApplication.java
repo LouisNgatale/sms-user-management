4c7acbfc3c9494e834c0d92c9ad019ede0455058
LouisNgatale <louisngatale@gmail.com>
2021-04-08 01:17:34 +0300

    Service discovery

--- src/main/java/i/louis/sms_management_service/SmsManagementServiceApplication.java
+++ src/main/java/i/louis/sms_management_service/SmsManagementServiceApplication.java
@@ -2,10 +2,10 @@ package i.louis.sms_management_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
 public class SmsManagementServiceApplication {
 
     public static void main(String[] args) {
